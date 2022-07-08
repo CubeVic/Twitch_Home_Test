96a24140d6bedfb05e0179d7c2422553c3234a3d
victor_blackPC <victoraguirre.f@gmail.com>
2022-07-08 17:38:18 +0800

    init

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from twitch_test import TestDrive
-
-browsers = TestDrive.Twitch(mobile_emulator={"deviceName": "iPhone XR"})
+mobile_emulation = {
+    "deviceMetrics": { "width": 375, "height": 812, "pixelRatio": 3.0 },
+    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
+}
+# mobile_emulator={"deviceName": "iPhone XR"}
+browsers = TestDrive.Twitch(mobile_emulator=mobile_emulation)
 
 browsers.go_landing_page()
 browsers.click_search_button()
--- twitch_test/TestDrive.py
+++ twitch_test/TestDrive.py
@@ -13,10 +13,12 @@ class Twitch:
 	def __init__(self, mobile_emulator):
 		self.service = Service(ChromeDriverManager().install())
 		options = Options()
+
 		self.mobile_emulator = mobile_emulator
 		options.add_experimental_option(name="mobileEmulation", value=self.mobile_emulator)
 		drive = webdriver.Chrome(service=self.service, options=options)
 		self._drive = drive
+		self.drive.implicitly_wait(60)
 
 	@property
 	def drive(self):
@@ -42,55 +44,33 @@ class Twitch:
 		switch_to_channels.click()
 
 	def find_user(self, user_name):
-		# user = self.drive.find_element(By.XPATH,'//div[@role="list"]')
-		# print(user.text)
-		users = self.drive.find_elements(By.XPATH,'//div[@role="list"]')
-		print(f"this are the users {users}")
-		names = list()
-		for user in users:
-			print(user.text)
-		# 	names.append(user.text)
-		# print(names)
-		# actions = ActionChains(self.drive)
-		# element = self.drive.find_element(By.XPATH, '//a')
-		# last = names.pop()
-		# last_element = self.drive.find_element(By.XPATH, f'//a[text()^={last}]')
-		# actions.click_and_hold(element).drag_and_drop(source=element,target=last_element).perform()
+		# find_first_video = self.drive.find_element(By.XPATH, "//div[@role='list']")
+		# print(find_first_video)
+		names = []
+		elements = []
+		for _ in range(2):
+			self.drive.find_element(By.XPATH, "//div[@role='list']//a").send_keys(Keys.END)
+			f = self.drive.find_elements(By.TAG_NAME, 'h4')
+			for i in f:
+				if i.text not in names:
+					names.append(i.text)
+					elements.append(i)
+				print(i.text)
+				# print(f"get the location {i.location}")
+		print(names)
+		# print(elements)
+		print(True if user_name in names else False)
+
+		el = self.drive.find_element(By.XPATH, "//p[@text()='Monster Hunter Rise']")
+		el.click()
 
 
 
-# options = Options()
+
+
 # user_agent_android = "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
 # user_agent_iPhone = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1'"
-# mobile_emulator = {"deviceName": "iPhone XR"}
-#
-# # mobile_emulator = {
-# #     "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
-# #     "userAgent": user_agent_iPhone }
-# # options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
-#
-# options.add_experimental_option("mobileEmulation", mobile_emulator)
-#
-# # service = "chromedriver_103.exe"
-#
-# service = Service(ChromeDriverManager().install())
-#
-#
-# # browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-# browser = webdriver.Chrome(service=service, options=options)
-#
-# browser.get('http://twitch.tv')
-# element = '/html/body/div/div/div/nav/div/div/div[2]/div/input[@type="search"]'
-#
-# el = '//div/a/button'
-# search = '//div/input'
-#
-# elem = browser.find_element(By.XPATH, el)  # Find the search box
-# elem.click()
-# elem = browser.find_element(By.XPATH, search)
-# elem.click()
-# elem.send_keys('Mounster hunter' + Keys.RETURN)
-# search_result = browser.find_element(By.XPATH, "//main/div/div/section")
+
 # search_result.screenshot("image2.png")
 #
 # browser.quit()
